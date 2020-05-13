9167620c28d04d23a8d9da6add26a80386ce9c7d
Will G Piro <willgpiro@gmail.com>
2020-05-13 16:24:56 -0700

    exercise model and test

---
+++ __tests__/exercises.test.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates an excercise via POST', () => {
+    return request(app)
+      .post('/api/v1/exercises')
+      .send({
+        name: 'Pull-ups',
+        description: 'Using a pronated grip, grasp the pull bar with a wider than shoulder width grip. Take a deep breath, squeeze your glutes and brace your abs. Depress the shoulder blades and then drive the elbows straight down to the floor while activating the lats. Pull your chin towards the bar until the lats are fully contracted, then slowly lower yourself back to the start position and repeat for the assigned number of repetitions.',
+        sets: 3,
+        reps: 5
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Pull-ups',
+          description: 'Using a pronated grip, grasp the pull bar with a wider than shoulder width grip. Take a deep breath, squeeze your glutes and brace your abs. Depress the shoulder blades and then drive the elbows straight down to the floor while activating the lats. Pull your chin towards the bar until the lats are fully contracted, then slowly lower yourself back to the start position and repeat for the assigned number of repetitions.',
+          sets: 3,
+          reps: 5,
+          __v: 0
+        });
+      });
+  });
+});
