b5dc09732072ba47376d71bb723b7b16e3a2bc0f
Will G Piro <willgpiro@gmail.com>
2020-05-15 15:43:55 -0700

    backend GET route complete

--- __tests__/exercises.test.js
+++ __tests__/exercises.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Exercise = require('../lib/models/Exercise.js');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -37,6 +38,30 @@ describe('app routes', () => {
           url: 'https://youtu.be/5oxviYmdHCY',
           __v: 0
         });
+      });
+  });
+  
+  it('retrieves a list of exercieses via GET', async() => {
+    await Exercise.create({
+      name: 'Pull-ups',
+      description: 'Using a pronated grip, grasp the pull bar with a wider than shoulder width grip. Take a deep breath, squeeze your glutes and brace your abs. Depress the shoulder blades and then drive the elbows straight down to the floor while activating the lats. Pull your chin towards the bar until the lats are fully contracted, then slowly lower yourself back to the start position and repeat for the assigned number of repetitions.',
+      sets: 3,
+      reps: 5,
+      url: 'https://youtu.be/5oxviYmdHCY',
+    });
+
+    return request(app)
+      .get('/api/v1/exercises')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.any(String),
+          name: 'Pull-ups',
+          description: 'Using a pronated grip, grasp the pull bar with a wider than shoulder width grip. Take a deep breath, squeeze your glutes and brace your abs. Depress the shoulder blades and then drive the elbows straight down to the floor while activating the lats. Pull your chin towards the bar until the lats are fully contracted, then slowly lower yourself back to the start position and repeat for the assigned number of repetitions.',
+          sets: 3,
+          reps: 5,
+          url: 'https://youtu.be/5oxviYmdHCY',
+          __v: 0
+        }]);
       });
   });
 });
